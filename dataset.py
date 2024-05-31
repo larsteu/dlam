af08fbe448490ed6c156b4dbf97bfa3a1f7d5087
lars.teubner <larsdamian01@t-online.de>
2024-05-31 13:01:13 +0200

    predicting now game results instead boolean game won label

--- dataset.py
+++ dataset.py
@@ -1,13 +1,16 @@
+import pandas as pd
 from torch.utils.data import Dataset
 from utils import normalize_dataset
 import numpy as np
 
 
 class EMDataset(Dataset):
-    def __init__(self, dataset, normalize=False):
+    def __init__(self, dataset: pd.DataFrame, normalize=False):
         self.dataset = dataset
         self.normalize = normalize
-
+        self.dataset["team_1_goals"] = self.dataset["game_result"].map(lambda x: int(x.split(sep="–")[0]))
+        self.dataset["team_2_goals"] = self.dataset["game_result"].map(lambda x: int(x.split(sep="–")[1]))
+        self.dataset = dataset.drop(columns="game_result")
         if self.normalize:
             self.dataset = normalize_dataset(
                 self.dataset, "data/normalization_info.json",
@@ -17,7 +20,8 @@ class EMDataset(Dataset):
         return int(len(self.dataset) / 52)
 
     def __getitem__(self, idx):
-        label = self.dataset.iloc[idx*52].values[-1]
-        data = self.dataset.drop(columns="game_won")
+        label_1 = self.dataset.iloc[idx*52].values[-2]
+        label_2 = self.dataset.iloc[idx*52].values[-1]
+        data = self.dataset.drop(columns=["team_1_goals", "team_2_goals"])
         data = data.iloc[idx * 52:(idx * 52) + 52].values
-        return data, np.array([label])
+        return data, np.array([label_1, label_2])
